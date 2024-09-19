d40f1c9a681d4970613c84d0c7fad412cfedd1b1
afif1731 <afifsdi@gmail.com>
2024-09-19 11:11:20 +0700

    fix: vt text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,13 +30,13 @@ export default function IndexPage() {
           </div>
           <Navbar/>
           <div className='absolute hidden md:block bottom-10 left-10 m-0'>
-            {vtStart && <p className=' text-white ptm-p2'>MOJOKERTO, JAWA TIMUR</p>}
+            {vtStart && <p className=' text-white ptm-p2 hidden'>MOJOKERTO, JAWA TIMUR</p>}
           </div>
           <div className=' absolute hidden md:block hover:scale-105 duration-300 bottom-10 right-10 m-0'>
               {
                 vtStart &&
                 <a href='https://psb.ponpestekmajapahit.sch.id' target='_blank' rel='noopener noreferrer' className=' flex align-middle items-center bg-[#D9D9D9] bg-opacity-30 rounded-full py-1 px-3 border border-white'>
-                    <span className=' text-white'>APPLY NOW</span>
+                    <span className=' text-white'>Yuk Bergabung!</span>
                 </a>
               }
           </div>
